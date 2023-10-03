3befe9e5078319d6ab80cb8db343d5972adecf47
Raghav Agrawal <raghav170101@gmail.com>
2023-10-03 17:39:50 +0530

    feat: coming soon

--- smartwood/src/components/home/display.tsx
+++ smartwood/src/components/home/display.tsx
@@ -1,25 +1,21 @@
 "use client";
 
-import ComingSoon from "react-coming-soon";
 import { Box, styled } from "@mui/material";
-
-const Root = styled(Box)`
-  font-family: "Roboto, sans-serif";
-  text-align: center;
-`;
 
 const Display = () => {
   return (
-    <Root>
-      <ComingSoon
-        title="We are left with few things"
-        subtitle="See you in"
-        bgColor="#2A2A29"
-        textColor="#FF7133"
-        date="Mon Oct 9 2023 17:00:00 GMT-0600 (Mountain Daylight Time)"
-        illustration="web-development"
-      />
-    </Root>
+    <Box
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        height: "100vh",
+        color: "black",
+        fontSize: "32px",
+      }}
+    >
+      Coming Soon
+    </Box>
   );
 };
 
